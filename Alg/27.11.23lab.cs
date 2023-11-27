3a47d2d784c18660ed07dace85247dd63d8f974a
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2023-11-27 14:20:34 +0600

    Update 27.11.23lab.cs

--- Alg/27.11.23lab.cs
+++ Alg/27.11.23lab.cs
@@ -1,1 +1,37 @@
-using System
+static void Main()
+{
+Car[] cars = new Car[3];
+cars[0] = new Car(2000, "синий","audi","Иванов И. И.", 2001);
+cars[1] = new Car(2002,"красный","Волга","Петров П. П.", 2003);
+cars[2] = new Car(2004,"серый","Нива","Сидоров С. С.", 2005);
+
+foreach (Car car in cars)
+{
+car.Printcl();
+}
+
+Console.WriteLine("Машины, выпущенные в 2004 году:");
+foreach (Car car in cars)
+{
+car.PrintByYear(2004);
+}
+
+Console.WriteLine("Машины марки Волга:");
+foreach (Car car in cars)
+{
+car.PrintByName("Волга");
+}
+
+Console.WriteLine("Машины с то в 2005 году:");
+foreach (Car car in cars)
+{
+car.PrintByTo(2005);
+}
+
+Console.WriteLine("Машины Иванова. И. И.");
+foreach (Car car in cars)
+{
+car.PrintByHold("Иванов И. И.");
+}
+}
+}
