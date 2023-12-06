2d014f05830fac41d4a1e5b61432d81d742ca618
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2023-12-06 20:50:01 +0600

    Update and rename 09.10mice.txt to 09.10mice.cs

--- Alg/09.10mice.cs
+++ Alg/09.10mice.cs
@@ -1,43 +1,36 @@
 дано к мышей из них 1 бел мыши сидят по кругу кот нач сьедать каждую мную с какой кот должен есть каждую м чтобы осталась белая
 полный перебор нельзя позицию белой задавать в нач массив нум мышей  с 0
 using System;
-using System.Linq;
-
-class CatAndMice {
+class Program {
     public static void Main(string[] args) {
         int K = Convert.ToInt32(Console.ReadLine());
         int M = Convert.ToInt32(Console.ReadLine());
-        int white = Convert.ToInt32(Console.ReadLine());
-
+        int w = Convert.ToInt32(Console.ReadLine());
         int[] mice = new int[K];
+
         for (int i = 0; i < K; i++) {
             mice[i] = 0;
         }
-        
-        int pointer = 0;
-        mice[pointer] = 1;
+        int mark = 0;
+        mice[mark] = 1;
 
         while (mice.Count(c => c == 0) > 0) {
-            int moved = 0;
-
-            while (moved != M) {
-                ++pointer;
+            int mov = 0;
+            while (mov != M) {
+                ++mark;
                 
-                if (pointer > K - 1) {
-                    pointer = 0;
+                if (mark > K - 1) {
+                    mark = 0;
                 }
 
-                if (mice[pointer] == 0) {
-                    ++moved;
+                if (mice[mark] == 0) {
+                    ++mov;
                 }
             }
-
-            mice[pointer] = 1;
+            mice[mark] = 1;
         }
-
-        int difference = pointer - white;
-        int answer = (K - difference) % K;
-
-        Console.WriteLine("Ответ: {0}", answer);
+        int dif = mark - w;
+        int res = (K - dif) % K;
+        Console.WriteLine("Начинать с: {0}", res);
     }
 }
