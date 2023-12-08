1a73399864792b602a8891515f025d2b9e1af0ac
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2023-12-08 19:23:58 +0600

    Update 27.11.23otr.cs

--- Alg/27.11.23otr.cs
+++ Alg/27.11.23otr.cs
@@ -1,8 +1,28 @@
-usung System;
-// Имеется трасса
-// Имеются дороги, которые соединяют нижний и верхний пути
-// Начальная точка
-// Финиш
-// n - количество интервалов
-// t - длина схода
-class Program {
+
+using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t;
+            int n;
+            int x = 0;
+            Console.WriteLine("t");
+         t=Convert.ToInt32(Console.ReadLine());
+            int y = t;
+            Console.WriteLine("n");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"a{i+1}, b{i+1}");
+                string ab = Console.ReadLine();
+                int a = Convert.ToInt32(ab.Split(' ')[0]);
+                int b = Convert.ToInt32(ab.Split(' ')[1]);
+                x = x + a;
+                y = Math.Min(x+t, y+b);
+            }
+            Console.WriteLine(Math.Min(x+t,y));
+            Console.ReadLine() 
+        }
+    }
+}
