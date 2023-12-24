6f946cd8e62c99c331ab1b7ee6c5086f858404df
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2023-12-24 15:28:48 +0600

    Update  mice2.cs

--- Alg/ mice2.cs
+++ Alg/ mice2.cs
@@ -10,57 +10,51 @@ class Program{
         int n1 = Convert.ToInt32(Console.ReadLine());
         int m1 = Convert.ToInt32(Console.ReadLine());
 
-        int remainder = n1 + m1;
+        int ost = n1 + m1;
 
         int[] mice = new int[n + m];
         for (int i = 0; i < mice.Length; i++) {
             mice[i] = 1;
         }
-
         int pointer = 0;
-
-        while (mice.Count(c => c == 1) > remainder) {
+        while (mice.Count(c => c == 1) > ost) {
             int moved = 0;
-
             while (moved != k) {
                 ++pointer;
 
                 if (pointer > mice.Length - 1) {
                     pointer = 0;
                 }
-
                 if (mice[pointer] == 1) {
                     ++moved;
                 }
             }
-
             mice[pointer] = 0;
         }
-
         int placed_n = 0;
         int placed_m = 0;
-        int placed_alive_n = 0;
-        int placed_alive_m = 0;
+        int placedalive_n = 0;
+        int placedalive_m = 0;
 
         if (n - n1 == 0 || m - m1 == 0) {
-            Console.WriteLine("Мышек так расположить не получится");
+            Console.WriteLine("Мышек расположить не получится.");
         } else {
             for (int i = 0; i < mice.Length; i++) {
                 if (mice[i] == 0) {
                     if (placed_n < n - n1) {
-                        Console.WriteLine("Серая");
+                        Console.WriteLine("Серая.");
                         placed_n++;
                     } else if (placed_m < m - m1) {
-                        Console.WriteLine("Белая");
+                        Console.WriteLine("Белая.");
                         placed_m++;
                     }
                 }  else if (mice[i] == 1) {
-                    if (placed_alive_n < n1) {
+                    if (placedalive_n < n1) {
                         Console.WriteLine("Серая");
-                        placed_alive_n++;
-                    } else if (placed_alive_m < m1) {
-                        Console.WriteLine("Белая");
-                        placed_alive_m++;
+                        placedalive_n++;
+                    } else if (placedalive_m < m1) {
+                        Console.WriteLine("Белая.");
+                        placedalive_m++;
                     }
                 } 
             }
