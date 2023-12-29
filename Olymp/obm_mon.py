7cad1bfe919bc45962b2dad4ffc59b23b8dcf727
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2023-12-29 19:28:37 +0600

    Update obm_mon.py

--- Olymp/obm_mon.py
+++ Olymp/obm_mon.py
@@ -1,1 +1,27 @@
-o
+l = [int(x) for x in input().split()][1:] 
+lunhap = sorted([int(x) for x in input().split()][1:], reverse=True)
+t = [int(x) for x in input().split()][1:] 
+tunhap = sorted([int(x) for x in input().split()][1:])
+moneyl = [int(x) for x in input().split()]
+realmon = 0
+
+for index, n in enumerate(moneyl):
+    for unhap in lunhap:
+        if n > unhap:
+            n -= 1
+    for perev in l[index:]:
+        n *= perev
+    realmon += n
+    
+c = []
+for perev in reversed(t):
+    c += [realmon % perev]
+    realmon //= perev
+c += [realmon]
+for index, n in enumerate(c):
+    for unhap in tunhap:
+        if n >= unhap:
+            n += 1
+    c[index] = n
+for i in reversed(c):
+    print(i, end=" ")
