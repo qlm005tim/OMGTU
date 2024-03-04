677b9a603d987c77a69288c4c064e11e3b80c82f
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2024-03-04 12:31:08 +0600

    Update trapezPracticum.cs

--- Discret/trapezPracticum.cs
+++ Discret/trapezPracticum.cs
@@ -6,14 +6,14 @@ class TrapezoidalRule {
       var answ=TrapezoidalRule.Solve(f, -3, 3,0.1);
       Console.WriteLine(answ); //answer
       
-      Func<double, double> f=(double x)=> -x*x+9;
-      Console.WriteLine(f(2.9)); //it is function no answer
+      //Func<double, double> f=(double x)=> -x*x+9;
+      //Console.WriteLine(f(2.9)); //it is function no answer
   }
     public static double Solve(Func<double, double>f, double a, double b, double dx){
         double nac_x=a;
         double s_trap=0;
         for (int i=0; i< (b-a)/dx; i++) {
-            si=(f(nac_x)+f(nac_x+dx))*0.5*dx;
+            double si=(f(nac_x)+f(nac_x+dx))*0.5*dx;
             nac_x=nac_x+dx;
             s_trap+=si;
         return s_trap;
