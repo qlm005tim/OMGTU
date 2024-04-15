64f63c81a3b7313ed7019fe5efed1b44df1932dc
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2024-04-15 11:16:17 +0600

    Update strSBregexp.cs

--- practicum/strSBregexp.cs
+++ practicum/strSBregexp.cs
@@ -54,9 +54,32 @@ class Program
     }
 
 
-
-
 using System;
-using System.Text;
+using System.Text.RegularExpressions;
 class Program
 {
+string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+var data = new string[]
+{
+    "tom@gmail.com",
+    "+12345678999",
+    "bob@yahoo.com",
+    "+13435465566",
+    "sam@yandex.ru",
+    "+43743989393"
+};
+ 
+Console.WriteLine("Email List");
+for(int i = 0; i < data.Length; i++)
+{
+    if (Regex.IsMatch(data[i], pattern, RegexOptions.IgnoreCase))
+    {
+        Console.WriteLine(data[i]);
+    }
+}
+
+
+
+string s = "456-435-2318";
+Regex regex = new Regex("[0-9]{3}-[0-9]{3}-[0-9]{4}");
