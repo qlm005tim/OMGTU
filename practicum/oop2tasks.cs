a112cfd61c6a40c49a2869c957271e58ae27da55
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2024-05-13 12:59:44 +0600

    Update oop2tasks.cs

--- practicum/oop2tasks.cs
+++ practicum/oop2tasks.cs
@@ -34,3 +34,97 @@ class TrapezoidalRule
 
 
 
+ Вот пример программы на C#, которая использует библиотеку Newtonsoft.Json для работы с JSON и LINQ для выполнения запросов. Программа вычисляет средний балл по каждому предмету, определяет студентов с максимальным средним баллом и группы с лучшим средним баллом по каждому предмету.
+```csharp
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string jsonInput = File.ReadAllText("input.json");
+        var inputData = JsonConvert.DeserializeObject<InputData>(jsonInput);
+
+        var gpaByDiscipline = CalculateGPAByDiscipline(inputData.Data);
+        var studentsWithHighestGPA = GetStudentsWithHighestGPA(inputData.Data);
+        var bestGroupsByDiscipline = GetBestGroupsByDiscipline(inputData.Data);
+
+        var outputData = new OutputData
+        {
+            Response = new List<object>
+            {
+                new { taskName = "CalculateGPAByDiscipline", data = gpaByDiscipline },
+                new { taskName = "GetStudentsWithHighestGPA", data = studentsWithHighestGPA },
+                new { taskName = "GetBestGroupsByDiscipline", data = bestGroupsByDiscipline }
+            }
+        };
+
+        string jsonOutput = JsonConvert.SerializeObject(outputData, Formatting.Indented);
+        File.WriteAllText("output.json", jsonOutput);
+    }
+
+    static Dictionary<string, double> CalculateGPAByDiscipline(List<DataItem> data)
+    {
+        return data
+            .GroupBy(d => d.Discipline)
+            .ToDictionary(
+                g => g.Key,
+                g => g.Average(d => d.Mark)
+            );
+    }
+
+    static List<string> GetStudentsWithHighestGPA(List<DataItem> data)
+    {
+        var studentsGPA = data
+            .GroupBy(d => d.Name)
+            .ToDictionary(
+                g => g.Key,
+                g => g.Average(d => d.Mark)
+            );
+
+        var maxGPA = studentsGPA.Max(sg => sg.Value);
+        return studentsGPA
+            .Where(sg => Math.Abs(sg.Value - maxGPA) < 1e-9)
+            .Select(sg => sg.Key)
+            .ToList();
+    }
+
+    static Dictionary<string, string> GetBestGroupsByDiscipline(List<DataItem> data)
+    {
+        return data
+            .GroupBy(d => d.Discipline)
+            .ToDictionary(
+                g => g.Key,
+                g => g
+                    .GroupBy(d => d.Group)
+                    .OrderByDescending(gg => gg.Average(d => d.Mark))
+                    .First()
+                    .Key
+            );
+    }
+}
+
+class InputData
+{
+    public string TaskName { get; set; }
+    public List<DataItem> Data { get; set; }
+}
+
+class DataItem
+{
+    public string Name { get; set; }
+    public string Group { get; set; }
+    public string Discipline { get; set; }
+    public double Mark { get; set; }
+}
+
+class OutputData
+{
+    public List<object> Response { get; set; }
+}
+```
+В этой программе предполагается, что входной JSON-файл называется "input.json", и результаты будут сохранены в файл "output.json". Вы можете изменить имена файлов в соответствии с вашими потребностями.
