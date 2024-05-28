d97e87e2fb99a8eeee393e66ce1bfb4b438e3734
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2024-05-28 23:25:52 +0600

    Create Floid.py

---
+++ Discret/Floid.py
@@ -0,0 +1,35 @@
+def floyd_warshall(graph):
+    
+    n = max(max(x[:2]) for x in graph)# колво вершин если нумерация с 1
+    
+    matr_of_dist = [[float('inf')] * n for i in range(n)]# Инициализация матрицы расстояний
+    
+    for i in range(n):
+        matr_of_dist[i][i] = 0
+    
+    for u, v, w in graph:
+        matr_of_dist[u-1][v-1] = w
+        #matr_of_dist[v-1][u-1] = w
+    
+    # Алгоритм Флойда-Уоршелла
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                matr_of_dist[i][j] = min(matr_of_dist[i][j], matr_of_dist[i][k] + matr_of_dist[k][j])
+
+    return matr_of_dist
+    
+graph = [
+            [1, 2, 1],
+            [1, 3, 4],
+            [2, 3, 2],
+            [2, 4, 5],
+            [3, 4, 1],
+            [3, 5, 3],
+            [4, 5, 2]
+            ]
+
+            # Вычисление матрицы расстояний
+matr_of_dist = floyd_warshall(graph)
+for strin in matr_of_dist:
+    print(strin)
