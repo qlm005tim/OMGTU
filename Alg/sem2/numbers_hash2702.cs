93e34824282f660ab00ce2c026de4b13dd95b08a
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2024-06-02 17:42:31 +0600

    Update numbers_hash2702.cs

--- Alg/sem2/numbers_hash2702.cs
+++ Alg/sem2/numbers_hash2702.cs
@@ -10,3 +10,119 @@ j/*
 чтобы подсчитать сумму минут необходимо с помощью словаря и хеш-таблицы (в качестве ключа - дата, в качестве значения - кол-во минут),
 на выходе выдать данные из словаря и из хеш-таблицы
 */
+
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+class Program {
+    
+  public static void Hashtable_answ()//метод с использованием хеш таблицы
+  {
+    Hashtable data = new Hashtable();
+
+    Queue<string> queue = new Queue<string>();
+
+    Console.WriteLine("Введите информацию по номерам, 'end' конец ввода");
+    Console.WriteLine("Введите номер, дату разговора, время начала разговора и кол-во минут разговора через пробел:");
+
+    string inf = Console.ReadLine();
+
+    while (inf != "end") 
+    {
+      Console.WriteLine("Введите номер, дату разговора, время начала разговора и кол-во минут разговора через пробел:");
+      
+      queue.Enqueue(inf);//информация по номеру добавляется в очередь
+
+      inf = Console.ReadLine();
+      
+    }
+
+    int ql=queue.Count;
+    for (int i = 0; i < ql; i += 1) 
+    {
+      string str = queue.Dequeue();//строка с информацией по номеру
+      string[] infs = str.Split();//массив с номером, дата...
+
+      string phone_num = infs[0];
+      double t = Convert.ToDouble(infs[3]);//колво минут
+
+      if (data.ContainsKey(phone_num)) 
+      {
+        data[phone_num] = Convert.ToDouble(data[phone_num]) + t;
+      } else 
+      {
+        data.Add(phone_num, t);
+      }
+
+      Console.WriteLine(str);//str
+    }
+
+    Console.WriteLine("Общая сумма минут разговора каждого номера:");
+
+    foreach (DictionaryEntry phone in data) //цикл по очереди
+    {
+      Console.WriteLine($"{phone.Key}: {phone.Value} всего минут");
+    }
+  }
+
+
+
+  public static void Dict_answ() 
+  {
+    var data = new Dictionary<string, double>();
+
+    Queue<string> queue = new Queue<string>();
+
+    Console.WriteLine("Введите информацию по номерам, 'end' конец ввода");
+    Console.WriteLine("Введите номер, дату разговора, время начала разговора и кол-во минут разговора через пробел:");
+
+    string inf = Console.ReadLine();//информация по номеру
+
+    while (inf != "end") 
+    {
+      Console.WriteLine("Введите номер, дату разговора, время начала разговора и кол-во минут разговора через пробел:");
+      queue.Enqueue(inf);
+
+      inf = Console.ReadLine();
+    }
+    
+    int ql=queue.Count;
+    for (int i = 0; i < ql; i += 1) 
+    {
+      string str = queue.Dequeue();//строка с информацией из очерди
+      string[] infs = str.Split();//массив с информацией
+
+      string phone_num = infs[0];
+      double t = Convert.ToDouble(infs[3]);//время разговора
+
+      if (data.ContainsKey(phone_num)) 
+      {
+        data[phone_num] = Convert.ToDouble(data[phone_num]) + t;
+      } else {
+        data.Add(phone_num, t);
+      }
+
+      Console.WriteLine(str);
+    }
+
+    Console.WriteLine("Общая сумма минут разговора каждого номера:");
+
+    foreach (var phone in data)
+    {
+      Console.WriteLine($"{phone.Key}: {phone.Value} всего минут");
+    }
+  }
+
+
+  public static void Main(string[] args) 
+  {
+    
+    Console.WriteLine("Решение через хеш-таблицу");
+    Hashtable_answ();
+    Console.WriteLine("Решение через словарь");
+    Dict_answ();
+    
+    }
+  }
