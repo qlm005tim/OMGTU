32ada29ebc684d7068b98d486f0fcd26a90b01e9
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2024-06-05 01:28:48 +0600

    Update 10.04files.cs

--- Alg/sem2/10.04files.cs
+++ Alg/sem2/10.04files.cs
@@ -15,50 +15,66 @@ using System.Text;
 using System.IO;
 
 class Program {
-  public static int CompareByDates(string v1, string v2) {
-    double date1 = Convert.ToDouble(v1.Split()[0]);
-    double date2 = Convert.ToDouble(v2.Split()[0]);
     
-    if (date1 > date2) {
+  public static int CompareDates(string s1, string s2) {
+    double date1 = Convert.ToDouble(s1.Split()[0]);//s1-year1 city1 country1
+    double date2 = Convert.ToDouble(s2.Split()[0]);
+    
+    if (date1 > date2) 
+    {
       return 1;
-    } else {
+    } 
+    else 
+    {
       return -1;
     }
   }
 
   public static void Main(string[] args) {
-    Console.WriteLine("Line");
-    List<string> file = File.ReadAllLines("./file.txt").ToList();
-    List<string> sorted_years = new List<string>(file);
-    sorted_years.Sort(CompareByDates);
-    List<string> sorted_cities = new List<string>(file);
-    sorted_cities.Sort((s1, s2) => s1.Split()[1].CompareTo(s2.Split()[1]));
+    
+    List<string> f = File.ReadAllLines("./file.txt").ToList();//read file in list with strs 'year city country'
+    
+    List<string> f_sortyears = new List<string>(f);//list with sort years copy for sort
+    
+    f_sortyears.Sort(CompareDates);//sort copied list
+    
+    
+    List<string> f_sortcity = new List<string>(f);//copy for sort by cities
+    
+    f_sortcity.Sort((s1, s2) => s1.Split()[1].CompareTo(s2.Split()[1]));//sort copied list
 
-    Console.WriteLine();
-    Console.WriteLine("Отсортировано по дате:");
-    using (StreamWriter sw = File.CreateText("./out_file_1.txt"))  {
-      foreach (var line in sorted_years) {
+
+    Console.WriteLine("Отсортировано по дате: ");
+    
+    using (StreamWriter sw = File.CreateText("./outfile1.txt")) 
+    {
+      foreach (var line in f_sortyears)
+      {
         Console.WriteLine(line);
         sw.WriteLine(line);
       }
     }
 
-    Console.WriteLine();
+
     Console.WriteLine("Отсортировано по городам:");
-    using (StreamWriter sw = File.CreateText("./out_file_2.txt"))  {
-      foreach (var line in sorted_cities) {
+    using (StreamWriter sw = File.CreateText("./outfile2.txt"))  
+    {
+      foreach (var line in f_sortcity) 
+      {
         string[] split_line = line.Split();
         Console.WriteLine($"{split_line[1]} {split_line[0]} {split_line[2]}");
         sw.WriteLine($"{split_line[1]} {split_line[0]} {split_line[2]}");
       }
     }
 
-    Console.WriteLine();
     Console.WriteLine("Введите название страны:");
     string country = Console.ReadLine();
-    using (StreamWriter sw = File.CreateText("./out_file_3.txt"))  {
-      foreach (var line in file) {
-        if (country == line.Split()[2]) {
+    using (StreamWriter sw = File.CreateText("./outfile3.txt"))  
+    {
+      foreach (var line in f) 
+      {
+        if (country == line.Split()[2]) 
+        {
           Console.WriteLine(line);
           sw.WriteLine(line);
         }
