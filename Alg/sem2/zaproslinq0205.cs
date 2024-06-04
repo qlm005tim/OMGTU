b844f4afd369b995d9ebc4cd530ea4c4f468f2eb
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2024-06-04 23:37:04 +0600

    Update zaproslinq0205.cs

--- Alg/sem2/zaproslinq0205.cs
+++ Alg/sem2/zaproslinq0205.cs
@@ -1,7 +1,55 @@
-
 //Дан массив, состоящий из элементов int типа. Необходимо написать запрос (на LINQ), который выдаёт элементы с последней цифрой кратной 3.
-
-
-
-
 //Дан массив, состоящий из элементов int типа. Необходимо написать запрос (на LINQ), который выдаёт элементы с чётной суммой цифр.
+using System;
+using System.Linq;
+
+class Program
+{
+    static int SumDig(int n)
+    {
+        int s = 0;
+        while (n != 0)
+        {
+            s += n % 10;
+            n /= 10;
+        }
+        return s;
+    }
+
+    static void Main()
+    {
+        int[] arr = {
+            123,
+            456,
+            789,
+            111,
+            222,
+            333,
+            444,
+            555,
+            666,
+            777,
+            888,
+            999,
+            23932,
+            222233,
+        };//examplr
+
+        var last3 = arr.Where(n => (n % 10) % 3 == 0);
+        
+        var sum2 = arr.Where(n => SumDig(n) % 2 == 0);
+
+        Console.WriteLine("Элементы, у которых последняя цифра кратна 3:");
+        foreach (var x in last3)
+        {
+            Console.WriteLine(x);
+        }
+
+        
+        Console.WriteLine("Элементы, у которых сумма цифр кратна 2:");
+        foreach (var x in sum2)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
