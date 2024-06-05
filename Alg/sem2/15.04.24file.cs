f5b42368fca1d5552b7d0118430853843df2b3f0
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2024-06-05 14:39:10 +0600

    Update 15.04.24file.cs

--- Alg/sem2/15.04.24file.cs
+++ Alg/sem2/15.04.24file.cs
@@ -49,3 +49,62 @@ class FileMerge
 }
 
 
+using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static int GetMinCount(string line)//min len 'a' substring
+    {
+        int count = 0;
+        int min_count = int.MaxValue;
+        
+        foreach (char c in line)
+        {
+            if (c == 'а')
+            {
+                count++;
+            }
+            
+            else
+            {
+                if (count > 0 && count < min_count)//obnov min count
+                {
+                    min_count = count;
+                }
+                count = 0;
+            }
+        }
+        return min_count;
+    }
+
+    static void Main()
+    {
+        using (StreamReader f = new StreamReader("input.txt"))//fail
+        {
+            int min_len = int.MaxValue;//min len substring with a
+            string min_str = "";// str with min 'a' substring
+
+            string line;
+            int min_count = int.MaxValue;
+            
+            while ((line = f.ReadLine()) != null)
+            {
+                int count = GetMinCount(line);
+
+                if (count > 0 && count < min_count)
+                {
+                    min_count = count;
+                }
+
+                if (min_count > 0 && min_count < min_len)
+                {
+                    min_len = min_count;
+                    min_str = line;
+                }
+            }
+            Console.WriteLine(min_str);
+        }
+    }
+}
