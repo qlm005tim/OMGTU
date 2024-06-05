34bddde8764f61c0d8d2109815b3e5c9e2b4e944
qlm005tim <145261563+qlm005tim@users.noreply.github.com>
2024-06-05 23:33:02 +0600

    Update kryskal.py

--- Discret/kryskal.py
+++ Discret/kryskal.py
@@ -1,5 +1,5 @@
 graph=[[15,1,2],[14,1,5],[23,1,4],[19,2,3],[16,2,4],[15,2,5],[14,3,5],[26,3,6],[25,4,5],[23,4,7],[20,4,8],[24,5,6],[27,5,8],[18,5,9],[14,7,8],[18,8,9]]
-//matrix smegnosti weight, start, finish
+#matrix smegnosti weight, start, finish
 def v(graph):
     v=[]
     for x in graph:
@@ -11,23 +11,29 @@ def v(graph):
     
 def kruskal(graph):
     res=0
-    m=[]
+    m=[]#vers inz rebram mod
+    mod=[]
     graph=sorted(graph,key=lambda x: x[0])
     
     m.append(graph[0][1])
     m.append(graph[0][2])
     res+=graph[0][0]
+    mod.append([graph[0][1], graph[0][2]])
     
-    while len(m)< len(v(graph)):
+    while len(m) < len(v(graph)):
         for x in graph:
             if x[1] in m and x[2] not in m:
                 m.append(x[2])
                 res+=x[0]
+                mod.append([x[1],x[2]])
                 break
             if x[1] not in m and x[2] in m:
                 m.append(x[1])
                 res+=x[0]
+                mod.append([x[1],x[2]])
                 break
-    return res
-    
-print(kruskal(graph))
+    return res, mod
+
+mod_weight, mod= kruskal(graph) 
+print(mod_weight)
+print(mod)
